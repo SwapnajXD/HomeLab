3910d71c3af9b640ddb3bcc085df59da5104b9b1
SwapnajXD <swapnaj0806@gmail.com>
2026-06-01 20:29:10 +0530

    feat: provision first terraform-managed s3 bucket on localstack (Day 17)

--- terraform/localstack/main.tf
+++ terraform/localstack/main.tf
@@ -9,19 +9,25 @@ terraform {
 }
 
 provider "aws" {
-  # 📍 These credentials do not need to be real since LocalStack accepts any strings
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
   region                      = "us-east-1"
-
-  # 🔐 Overrides default production cloud credential validation checks
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
-  # 🚀 Redirects API workflows directly across Tailscale to your Ubuntu VM endpoint
   endpoints {
     s3       = "http://100.117.35.70:4566"
     dynamodb = "http://100.117.35.70:4566"
+  }
+}
+
+resource "aws_s3_bucket" "homelab_storage" {
+  bucket = "tf-homelab-storage-bucket"
+
+  tags = {
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+    Owner       = "Swapnaj"
   }
 }
